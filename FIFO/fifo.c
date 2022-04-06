0b4e51a27f4292b4b1fca379ca72f7332731fe07
Karol Borecki <karol.borecki@onet.eu>
2022-04-06 18:02:58 +0200

    Implemented Initzialization method

--- FIFO/fifo.c
+++ FIFO/fifo.c
@@ -1,1 +1,10 @@
 #include "fifo.h"
+
+fifo_t initzialize_fifo(){
+  fifo_t fifo = malloc(sizeof(*fifo));
+  fifo->queue = malloc(START_QUEUE_SIZE * sizeof(*(fifo->queue)));
+  fifo->real_size = START_QUEUE_SIZE;
+  fifo->size = 0;
+
+  return fifo;
+}
