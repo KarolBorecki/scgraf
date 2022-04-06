d7b0e70a85419769bea82380d043db8619939e51
Karol Borecki <karol.borecki@onet.eu>
2022-04-06 18:07:08 +0200

    Added main.c - FIFO test file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "FIFO/fifo.h"
+
+int main(int argc, char** argv){
+  printf("Hello World \n");
+
+  fifo_t fifo = initzialize_fifo();
+
+  return 1;
+}
