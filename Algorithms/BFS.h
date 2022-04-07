cffaa39985e63cb26dbdb763ba514a23eaf426cb
Karol Borecki <karol.borecki@onet.eu>
2022-04-07 17:22:14 +0200

    Fixed compilation issues

--- Algorithms/BFS.h
+++ Algorithms/BFS.h
@@ -1,10 +1,13 @@
 #ifndef _BFS_H_
 #define _BFS_H_
 
-int bfs(graph_t, node_t);
-int was_visited(fifo_t, unsigned int );
-void add_connections_of_node_to_queue(fifo_t, node_t);
-int is_all_nodes_visited(graph_t, fifo_t);
+#include "../FIFO/fifo.h"
+#include "../graph/graph.h"
+
+int bfs(graph_t graph, node_t start_node);
+int was_visited(fifo_t q, unsigned int node);
+void add_connections_of_node_to_queue(fifo_t q, node_t node);
+int is_all_nodes_visited(graph_t g, fifo_t q);
 
 
 #endif /* _BFS_H_ */
