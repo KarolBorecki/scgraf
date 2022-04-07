4289f04a43ce7601e659a610a0fadf03cbc633ee
Karol Borecki <karol.borecki@onet.eu>
2022-04-07 16:30:23 +0200

    Implemented path add function and enlarge functions

--- GraphHandler/GraphHandler.c
+++ GraphHandler/GraphHandler.c
@@ -8,11 +8,13 @@ graph_t initzialize_graph(unsigned int nodes_count){
 }
 
 void enlarge_nodes_memory(graph_t graph, unsigned int enlrage_multiplier){
-
+  graph->memory_size *= enlrage_multiplier;
+  graph->nodes = realloc(graph->nodes, graph->memory_size);
 }
 
 void enlarge_paths_memory(node_t node, unsigned int enlrage_multiplier){
-
+  node->memory_size *= enlrage_multiplier;
+  node->paths = realloc(node->paths, node->memory_size);
 }
 
 void clean_graph(graph_t graph){
@@ -34,7 +36,14 @@ void add_node(graph_t graph){
 }
 
 void add_path(node_t node, unsigned int connection, unsigned int value){
+  if(node->paths_count >= node->paths_memory_size)
+    enlarge_paths_memory(node, GRAPH_ENLARGE_MULTIPLIER);
 
+  path_t new_path = node->paths+node->paths_count;
+  new_path->connection = connection;
+  new_path->value = value;
+
+  node->paths_count++;
 }
 
 node_t get_node_with_index(graph_t graph, unsigned int index){
