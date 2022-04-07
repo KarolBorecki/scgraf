63a3da5d9eaf4d4f57679560448aa94339548342
Karol Borecki <karol.borecki@onet.eu>
2022-04-07 14:51:52 +0200

    Added init function

--- GraphHandler/GraphHandler.c
+++ GraphHandler/GraphHandler.c
@@ -1,1 +1,35 @@
 #include "GraphHandler.h"
+
+graph_t initzialize_graph(){
+  graph_t graph = malloc(sizeof(*graph));
+  graph->size = 0;
+  graph->nodes = initzialize_nodes(START_GRAPH_NODES_COUNT);
+  graph->memory_size = START_GRAPH_NODES_COUNT;
+}
+
+node_t initzialize_nodes(unsigned int size){
+  node_t nodes = malloc(size * sizeof(*nodes)));
+  for(int i=0; i<size; i++){
+    (nodes+i)->index = i;
+    (nodes+i)->paths = initzialize_paths(START_NODES_PATHS_COUNT);
+    (nodes+i)->paths_count = START_NODES_PATHS_COUNT;
+  }
+
+  return nodes;
+}
+
+path_t initzialize_paths(unsigned int size){
+  path_t paths = malloc(size * sizeof(*paths));
+  for(int i=0; i<size; i++){
+    (paths+i)->connection = NULL;
+    (paths+i)->value = 0
+  }
+}
+
+void clean_graph(graph_t graph);
+void add_node(graph_t graph, path_t paths, unsigned int paths_count);
+void add_path(node_t node, node_t connection, unsigned int value);
+
+node_t get_node_with_index(graph_t graph, unsigned int index);
+
+void print_graph(graph_t);
--- GraphHandler/GraphHandler.h
+++ GraphHandler/GraphHandler.h
@@ -24,9 +24,16 @@ typedef struct path{
 } *path_t;
 
 graph_t initzialize_graph();
+node_t initzialize_nodes(unsigned int size);
+path_t initzialize_paths(unsigned int size);
+
 void clean_graph(graph_t graph);
+
 void add_node(graph_t graph, path_t paths, unsigned int paths_count);
+void add_path(node_t node, node_t connection, unsigned int value);
 
 node_t get_node_with_index(graph_t graph, unsigned int index);
+
+void print_graph(graph_t);
 
 #endif /* _GRAPH_HANDLER_H_ */
