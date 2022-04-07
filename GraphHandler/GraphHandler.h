dbb422f18d5e9ca26d1fa5bdb02c3c579f609c84
Karol Borecki <karol.borecki@onet.eu>
2022-04-07 12:52:57 +0200

    Added graph methods prototypes

--- GraphHandler/GraphHandler.h
+++ GraphHandler/GraphHandler.h
@@ -1,6 +1,11 @@
 #ifndef _GRAPH_HANDLER_H_
 #define _GRAPH_HANDLER_H_
 
+#define START_GRAPH_NODES_COUNT 20
+#define START_NODES_PATHS_COUNT 4
+
+#define GRAPH_ENLARGE_MULTIPLIER 2
+
 typedef struct graph{
   node_t nodes;
   unsigned int size;
@@ -8,6 +13,7 @@ typedef struct graph{
 } *graph_t;
 
 typedef struct node{
+  unsigned int index; /* index in array of graph_t->nodes */
   path_t paths;
   unsigned int paths_count;
 } *node_t;
@@ -17,5 +23,10 @@ typedef struct path{
   unsigned int value;
 } *path_t;
 
+graph_t initzialize_graph();
+void clean_graph(graph_t graph);
+void add_node(graph_t graph, path_t paths, unsigned int paths_count);
+
+node_t get_node_with_index(graph_t graph, unsigned int index);
 
 #endif /* _GRAPH_HANDLER_H_ */
