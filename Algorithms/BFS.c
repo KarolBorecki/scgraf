1fe264f882e9a655760cf5134603a2449271f686
Karol Borecki <karol.borecki@onet.eu>
2022-04-07 14:34:05 +0200

    Implemented is_all_nodes_visited

--- Algorithms/BFS.c
+++ Algorithms/BFS.c
@@ -10,6 +10,8 @@ int bfs(graph_t graph, node_t start_node){
     add_connections_of_node_to_queue(q, actual_node);
     pop(q);
   }
+
+  return is_all_nodes_visited(graph, q);
 }
 
 int was_visited(fifo_t q, unsigned int node){
@@ -23,4 +25,12 @@ void add_connections_of_node_to_queue(fifo_t q, node_t node){
     unsigned int connection_index = node->(paths+i)->connection->index;
     if(was_visited(q, connection_index) <= 0) push(q, connection_index);
   }
+}
+//TODO it is O(n^2) - try to do it better
+int is_all_nodes_visited(graph_t g, fifo_t q){
+  return (g->size - head_index(q) + 1) == 0 ? 1 : 0; /* this is probapbly enough, but more tests are needed */
+  for(int i=0; i<head_index(q); i++)
+    if(was_visited(get_node_with_index(fifo, i)) <= 0)
+      return 0;
+  return 1;
 }
