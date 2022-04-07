bfc532ff5036e095cf6743022bc990d5e4758575
Karol Borecki <karol.borecki@onet.eu>
2022-04-07 12:22:54 +0200

    Moved graph structs to *.h file

--- GraphHandler/GraphHandler.h
+++ GraphHandler/GraphHandler.h
@@ -1,5 +1,21 @@
 #ifndef _GRAPH_HANDLER_H_
 #define _GRAPH_HANDLER_H_
 
+typedef struct graph{
+  node_t nodes;
+  unsigned int size;
+  unsigned int memory_size;
+} *graph_t;
+
+typedef struct node{
+  path_t paths;
+  unsigned int paths_count;
+} *node_t;
+
+typedef struct path{
+  node_t connection;
+  unsigned int value;
+} *path_t;
+
 
 #endif /* _GRAPH_HANDLER_H_ */
