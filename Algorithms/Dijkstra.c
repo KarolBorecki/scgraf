6d0170982917bbc58a472d81ae85ad692c7e0407
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2022-04-08 00:00:53 +0200

    implementation of bases for Dijkstras algorithm

--- Algorithms/Dijkstra.c
+++ Algorithms/Dijkstra.c
@@ -1,1 +1,50 @@
 #include "Dijkstra.h"
+#include <stdio.h>
+
+#include "../FIFO/fifo.h"
+#include "../graph/graph.h"
+
+
+
+void initialize_start_table(table_t table, graph_t graph, node_t start_node){
+    table->size= graph->size;
+
+    table->nodes_indexes= malloc(sizeof(graph->nodes->index) * graph->size);
+    if(table->nodes_indexes == NULL)
+        return;
+
+    table->previous_nodes= malloc(sizeof(graph->nodes->index) * graph->size);
+    if(table->previous_nodes == NULL)
+        return;
+
+    table->shortest_distances= malloc(sizeof(graph->nodes->paths->value) * graph->size);
+    if(table->shortest_distances == NULL)
+        return;
+
+    for(int i= 0; i<graph->size; i++) {
+        if(graph->nodes[i].index != start_node->index)
+            table->shortest_distances[i] = DBL_MAX;
+        else
+            table->shortest_distances[i] = 0;
+        table->nodes_indexes[i] = graph->nodes[i].index;
+        table->previous_nodes[i] = INVALID_NODE;
+    }
+}
+
+void print_table(table_t table){
+    fprintf(stdout, "\n=======DIJKSTRA' S TABLE========\n\n"
+                    "NODE_index\tSHORTEST_PATH\tPREV_NODE\n");
+    for(int i= 0; i<table->size; i++)
+        fprintf(stdout, "%d\t%lf\t%d",
+                        table->nodes_indexes[i],
+                        table->shortest_distances[i],
+                        table->previous_nodes[i]);
+}
+
+void free_table(table_t table){
+    free(table->shortest_distances);
+    free(table->previous_nodes);
+    free(table->nodes_indexes);
+
+    free(table);
+}
--- Algorithms/Dijkstra.h
+++ Algorithms/Dijkstra.h
@@ -1,5 +1,20 @@
 #ifndef _DIJKSTRA_H_
 #define _DIJKSTRA_H_
 
+#include <float.h>
+#include <limits.h>
+
+#define INVALID_NODE UINT_MAX
+
+typedef struct table{
+    unsigned int    size;
+    unsigned int    *nodes_indexes;
+    unsigned int    *previous_nodes;
+    unsigned double *shortest_distances;
+} *table_t;
+
+void initialize_start_table(table_t, graph_t, node_t);
+void print_table(table_t);
+void free_table(table_t);
 
 #endif /* _DIJKSTRA_H_ */
