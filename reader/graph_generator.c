55d5ea5ba215730b69db7f3b0b85fdc39d1a7645
Karol Borecki <karol.borecki@onet.eu>
2022-04-09 22:48:02 +0200

    Implemented graph generating

--- reader/graph_generator.c
+++ reader/graph_generator.c
@@ -1,1 +1,28 @@
 #include "graph_generator.h"
+
+#include <time.h>
+#include <stdlib.h>
+
+#define GENERATED_NODE_CONNECTIONS_COUNT 4
+#define GENERATED_PATH_MAX_VALUE 10
+
+graph_t generate_graph(unsigned int size){
+  srand(time(NULL));
+
+  graph_t graph = initzialize_graph(size);
+
+  for(int i<0; i<size; i++)
+    generate_new_node(graph);
+
+  return graph;
+}
+
+void generate_new_node(graph_t graph){
+  node_t new_node = graph_add_node(graph);
+  for(int i=0; i<GENERATED_NODE_CONNECTIONS_COUNT; i++)
+    generate_new_path(graph, new_node);
+}
+
+void generate_new_path(graph_t graph, node_t node){
+  graph_add_path(node, rand() % graph_size(graph), rand() % 10);
+}
--- reader/graph_generator.h
+++ reader/graph_generator.h
@@ -3,5 +3,8 @@
 
 #include "../graph/graph.h"
 
+graph_t generate_graph(unsigned int size);
+void generate_new_node(graph_t graph, unsigned int index);
+void generate_new_path(node_t node);
 
 #endif /* _GRAPH_GENERATOR_H_ */
