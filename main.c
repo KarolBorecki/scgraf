7e2a326ea909c889a9621edaa360bf27398e5c80
Karol Borecki <karol.borecki@onet.eu>
2022-04-09 14:28:11 +0200

    Added example graph and diagrams for it

--- main.c
+++ main.c
@@ -4,35 +4,63 @@
 #include "graph/graph.h"
 #include "Algorithms/BFS.h"
 
-int main(int argc, char** argv){
-  printf("Graph Handler Tester\n");
-
-  graph_t g = initzialize_graph(5);
+graph_t generate_example_graph(){
+  graph_t g = initzialize_graph(9);
 
   node_t help_node;
 
   //Node 0
   help_node = graph_add_node(g);
-  graph_add_path(help_node, 1, 0.1);
-  //graph_add_path(help_node, 2, 0.4);
-  graph_add_path(help_node, 3, 0.23);
+  graph_add_path(help_node, 1, 1.4);
 
   //Node 1
   help_node = graph_add_node(g);
-  graph_add_path(help_node, 0, 0.11);
-  //graph_add_path(help_node, 2, 0.24);
-  graph_add_path(help_node, 3, 1.21243);
+  graph_add_path(help_node, 2, 0.1);
+  graph_add_path(help_node, 4, 2.3);
 
   //Node 2
   help_node = graph_add_node(g);
+  graph_add_path(help_node, 1, 0.1);
+  graph_add_path(help_node, 5, 0.1);
 
   //Node 3
   help_node = graph_add_node(g);
-  graph_add_path(help_node, 2, 3.697);
-  graph_add_path(help_node, 0, 0.64689);
-  graph_add_path(help_node, 1, 3.2343);
+  graph_add_path(help_node, 4, 1.3);
+  graph_add_path(help_node, 6, 0.1);
 
-  print_graph(g);
-  printf("BFS = %d\n\n", bfs(g, 0));
+  //Node 4
+  help_node = graph_add_node(g);
+  graph_add_path(help_node, 1, 2.3);
+  graph_add_path(help_node, 5, 2.0);
+  graph_add_path(help_node, 7, 3.0);
+  graph_add_path(help_node, 3, 1.3);
+
+  //Node 5
+  help_node = graph_add_node(g);
+  graph_add_path(help_node, 2, 0.1);
+  graph_add_path(help_node, 4, 2.0);
+
+  //Node 6
+  help_node = graph_add_node(g);
+  graph_add_path(help_node, 3, 0.1);
+  graph_add_path(help_node, 7, 0.2);
+
+  //Node 7
+  help_node = graph_add_node(g);
+  graph_add_path(help_node, 4, 3.0);
+  graph_add_path(help_node, 6, 0.2);
+  graph_add_path(help_node, 8, 1.0);
+
+  //Node 8
+  help_node = graph_add_node(g);
+  graph_add_path(help_node, 7, 1.0);
+
+  return g;
+}
+
+int main(int argc, char** argv){
+  graph_t graph = generate_example_graph();
+
+  print_graph(graph);
   return 1;
 }
