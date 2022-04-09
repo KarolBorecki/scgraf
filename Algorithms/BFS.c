e771deea8f85af24beb79cc1ecee03e2d7b4e77e
Karol Borecki <karol.borecki@onet.eu>
2022-04-09 12:12:57 +0200

    Optimized is_all_nodes_visited, refactored code

--- Algorithms/BFS.c
+++ Algorithms/BFS.c
@@ -10,27 +10,24 @@ int bfs(graph_t graph, unsigned int start_node_index){
   fifo_t q = initzialize_fifo();
   fifo_push(q, start_node->index);
   node_t actual_node;
+
   while(fifo_is_empty(q) <= 0){
-    print_fifo(q);
     actual_node = graph_get_node_with_index(graph, fifo_peek(q));
-    add_connections_of_node_to_queue(q, actual_node);
+    bfs_add_distinct_connections_of_node_to_queue(q, actual_node);
     fifo_pop(q);
   }
-  return is_all_nodes_visited(graph, q);
+  return bfs_is_all_nodes_visited(graph, q);
 }
 
-void add_connections_of_node_to_queue(fifo_t q, node_t node){
+void bfs_add_distinct_connections_of_node_to_queue(fifo_t q, node_t node){
   for(int i=0; i<node->paths_count; i++){
     unsigned int connection_index = (node->paths+i)->connection;
     if(fifo_queue_contains_value(q, connection_index) == 0)
       fifo_push(q, connection_index);
   }
 }
-//TODO it is O(n^2) - try to do it better
-int is_all_nodes_visited(graph_t g, fifo_t q){
-  //return fifo_queue_size(q) == 0 ? 1 : 0; /* this is probapbly enough, but more tests are needed */
-  for(int i=0; i<fifo_queue_size(q); i++)
-    if(fifo_contains_value(q, i, 0, fifo_head_index(q)) <= 0)
-      return 0;
-  return 1;
+
+int bfs_is_all_nodes_visited(graph_t g, fifo_t q){
+  /* this is probapbly enough, but more tests are needed */
+  return (fifo_queue_size(q) - graph_size(g)) == 0 : 1 : 0;
 }
