b500557776648263ba2b698898c88810cb41c29b
Karol Borecki <karol.borecki@onet.eu>
2022-04-10 14:33:50 +0200

    Introduced main menu printer

--- main.c
+++ main.c
@@ -1,67 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "main_menu_printer.h"
 #include "graph/graph.h"
 #include "algorithms/bfs.h"
 #include "reader/graph_generator.h"
 
-graph_t generate_example_graph(){
-  graph_t g = initzialize_graph(9);
-
-  node_t help_node;
-
-  //Node 0
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 1, 1.4);
-
-  //Node 1
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 2, 0.1);
-  graph_add_path(help_node, 4, 2.3);
-
-  //Node 2
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 1, 0.1);
-  graph_add_path(help_node, 5, 0.1);
-
-  //Node 3
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 4, 1.3);
-  graph_add_path(help_node, 6, 0.1);
-
-  //Node 4
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 1, 2.3);
-  graph_add_path(help_node, 5, 2.0);
-  graph_add_path(help_node, 7, 3.0);
-  graph_add_path(help_node, 3, 1.3);
-
-  //Node 5
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 2, 0.1);
-  graph_add_path(help_node, 4, 2.0);
-
-  //Node 6
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 3, 0.1);
-  graph_add_path(help_node, 7, 0.2);
-
-  //Node 7
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 4, 3.0);
-  graph_add_path(help_node, 6, 0.2);
-  graph_add_path(help_node, 8, 1.0);
-
-  //Node 8
-  help_node = graph_add_node(g);
-  graph_add_path(help_node, 7, 1.0);
-
-  return g;
-}
-
 int main(int argc, char** argv){
-  graph_t graph = generate_graph(5);
+  print_greetings();
 
-  print_graph(graph);
   return 1;
 }
---
+++ main_menu_printer.h
@@ -0,0 +1,8 @@
+#ifndef _MENU_PRINTER_H_
+#define _MENU_PRINTER_H_
+
+void print_greetings();
+void print_help();
+void print_error();
+
+#endif /* _MENU_PRINTER_H_ */
