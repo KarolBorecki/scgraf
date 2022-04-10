8160c16318b4a61fe2f462d2e9cea91e9ae71c8c
Karol Borecki <karol.borecki@onet.eu>
2022-04-10 17:39:54 +0200

    implemented function prototypes

--- solver/graph_solver.c
+++ solver/graph_solver.c
@@ -1,1 +1,7 @@
 #include "graph_solver.h"
+
+void solver_get_shortest_path(graph_t graph, unsigned from, unsigned to);
+
+void solver_check_graph_consistency(graph_t graph);
+
+graph_t solver_divide_graph_into_n_graphs(graph_t graph, unsigned n); 
--- solver/graph_solver.h
+++ solver/graph_solver.h
@@ -1,5 +1,11 @@
 #ifndef _SOLVER_H_
 #define _SOLVER_H_
 
+void solver_get_shortest_path(graph_t graph, unsigned from, unsigned to);
+
+void solver_check_graph_consistency(graph_t graph);
+
+graph_t solver_divide_graph_into_n_graphs(graph_t graph, unsigned n); 
+
 
 #endif /* _SOLVER_H_ */
