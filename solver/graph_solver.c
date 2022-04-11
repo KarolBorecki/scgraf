be812bc1b8190b5ae2772f5e47175ae2ef628d50
Karol Borecki <karol.borecki@onet.eu>
2022-04-11 19:03:11 +0200

    Fixed solver for new graph_divider

--- solver/graph_solver.c
+++ solver/graph_solver.c
@@ -2,6 +2,8 @@
 
 #include "../graph/graph.h"
 #include "../algorithms/bfs.h"
+#include "../algorithms/dijkstra.h"
+#include "../algorithms/graph_divider.h"
 
 void solver_get_shortest_path(graph_t graph, unsigned from, unsigned to){
   printf("SHORTEST PATH");
@@ -13,8 +15,6 @@ void solver_check_graph_consistency(graph_t graph){
   else printf("GRAF niespojny!\n");
 }
 
-graph_t solver_divide_graph_into_n_graphs(graph_t graph, unsigned n){
-  graph_t g = initzialize_graph(0);
-  printf("DIVIDE GRAPH");
-  return g;
+void solver_divide_graph_into_n_graphs(graph_t graph, unsigned divides){
+  divide_graph(graph, divides);
 }
--- solver/graph_solver.h
+++ solver/graph_solver.h
@@ -9,7 +9,7 @@ void solver_get_shortest_path(graph_t graph, unsigned from, unsigned to);
 
 void solver_check_graph_consistency(graph_t graph);
 
-graph_t solver_divide_graph_into_n_graphs(graph_t graph, unsigned n);
+void solver_divide_graph_into_n_graphs(graph_t graph, unsigned divides);
 
 
 #endif /* _SOLVER_H_ */
